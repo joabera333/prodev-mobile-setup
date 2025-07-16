25b53d27069aebde1042dbc8c052bca87d16fdc4
jo_abera <johannesab0939@gmail.com>
2025-07-16 16:21:33 +0300

    Navigation using Tabs Navigation Patterns

---
+++ prodev-mobile-app-0x05/app/(home)/inbox.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+const InboxScreen = () => {
+	return (
+		<View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+			<Text>Inbox Screen</Text>
+		</View>
+	);
+};
+
+export default InboxScreen;
